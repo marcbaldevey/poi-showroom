6d617ba2ac51c0d449cc354f46fcedd80c164020
marcbaldevey <marcbaldevey@inedit.com>
2025-01-14 11:26:28 +0100

    [sc-47610] [bug] POI => inèdit connect (25.1.0-b.2)

--- src/components/Step3.vue
+++ src/components/Step3.vue
@@ -1,5 +1,10 @@
 <template>
-  <Step :step-number="3" title="Send the order" description="Finish your order and send it to POI, you will be able to find it in your workflow." img-src="/send.svg" />
+  <Step
+    :step-number="3"
+    title="Send the order"
+    description="Finish your order and send it to Inèdit Connect, you will be able to find it in your workflow."
+    img-src="/send.svg"
+  />
 </template>
 <script lang="ts" setup>
 import Step from "./Step.vue";
