d948ab604e4c497e65c93d17b258d34451f99023
marcbaldevey <marcbaldevey@inedit.com>
2026-02-23 12:41:38 +0100

    Remove redundant logo from Navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,6 @@
 <template>
   <nav class="h-24 bg-white flex items-end pl-2">
     <img src="/inedit_logo_gris.png" alt="" class="cursor-pointer h-20 p-4 pb-8" @click="router.replace('/')" />
-    <img src="/efi_reggiani_logo.png" alt="" class="cursor-pointer h-24 p-1" @click="router.replace('/')" />
   </nav>
 </template>
 <script lang="ts" setup>
